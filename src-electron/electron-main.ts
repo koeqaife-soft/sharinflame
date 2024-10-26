2c851d00b5b05be77e8aee1e96b83855991c5e46
Danik <retikadni@gmail.com>
2024-10-26 13:24:34 +0200

    Electron

---
+++ src-electron/electron-main.ts
@@ -0,0 +1,48 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import os from "os";
+
+const platform = process.platform || os.platform();
+
+let mainWindow: BrowserWindow | undefined;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    icon: path.resolve(__dirname, "icons/icon.png"),
+    width: 1000,
+    height: 600,
+    useContentSize: true,
+    webPreferences: {
+      contextIsolation: true,
+      preload: path.resolve(__dirname, process.env.QUASAR_ELECTRON_PRELOAD)
+    }
+  });
+
+  mainWindow.loadURL(process.env.APP_URL);
+
+  if (process.env.DEBUGGING) {
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.webContents.on("devtools-opened", () => {
+      mainWindow?.webContents.closeDevTools();
+    });
+  }
+
+  mainWindow.on("closed", () => {
+    mainWindow = undefined;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === undefined) {
+    createWindow();
+  }
+});
