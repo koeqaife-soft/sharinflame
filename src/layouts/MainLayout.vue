de41e2f8fdb08e68710ac6f1d96cb22615bef94e
Danik <retikadni@gmail.com>
2024-10-27 16:22:37 +0100

    Use component for logo

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,18 +2,7 @@
   <q-layout view="lHh Lpr lFf">
     <q-header unelevated>
       <q-toolbar class="toolbar">
-        <svg viewBox="-2.4 -2.4 28.8 28.8" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon">
-          <defs>
-            <mask id="a">
-              <rect x="-2.4" y="-2.4" width="28.8" height="28.8" rx="14.4" fill="#fff" />
-              <path
-                d="M12 21c4.418 0 8-3.356 8-7.496 0-3.741-2.035-6.666-3.438-8.06-.26-.258-.694-.144-.84.189-.748 1.69-2.304 4.123-4.293 4.123-1.232.165-3.112-.888-1.594-6.107.137-.47-.365-.848-.749-.534C6.905 4.905 4 8.511 4 13.504 4 17.644 7.582 21 12 21"
-                fill="#000"
-              />
-            </mask>
-          </defs>
-          <rect x="-2.4" y="-2.4" width="28.8" height="28.8" rx="14.4" fill="currentColor" mask="url(#a)" />
-        </svg>
+        <LogoComponent />
 
         <q-toolbar-title> SharinFlame </q-toolbar-title>
 
@@ -27,4 +16,6 @@
   </q-layout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LogoComponent from "src/components/LogoComponent.vue";
+</script>
