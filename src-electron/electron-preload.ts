a986d1366d3dba9b4939545b75304fe86140974f
Danik <retikadni@gmail.com>
2024-10-30 22:49:35 +0100

    Electron IPC funcs

--- src-electron/electron-preload.ts
+++ src-electron/electron-preload.ts
@@ -27,3 +27,11 @@
  *   }
  * }
  */
+
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("electron", {
+  minimizeWindow: () => ipcRenderer.send("minimize-window"),
+  maximizeWindow: () => ipcRenderer.send("maximize-window"),
+  closeWindow: () => ipcRenderer.send("close-window")
+});
