8b17d1a20cba349ca225069b591174a161851ad4
Danik <retikadni@gmail.com>
2024-10-31 16:15:48 +0100

    Post type

--- src/api/api.d.ts
+++ src/api/api.d.ts
@@ -5,3 +5,23 @@ interface ApiResponse<T = { [key: string]: unknown }> {
 }
 
 type ResponseWithAccess = ApiResponse<{ access: string }>;
+
+interface Post {
+  post_id: number;
+  user_id: number;
+  content: string;
+  created_at: string;
+  updated_at: string;
+  likes_count: number;
+  dislikes_count: number;
+  comments_count: number;
+  tags: string[];
+  media: string[];
+  status: string;
+  is_deleted: boolean;
+
+  created_at_unix: number;
+  updated_at_unix: number;
+}
+
+type ResponseWithPost = ApiResponse<Post>;
