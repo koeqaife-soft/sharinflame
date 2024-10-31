88f4f9d941f9ce091b3cd9177227fc679eb14d7a
Danik <retikadni@gmail.com>
2024-11-01 00:05:00 +0100

    Posts store

---
+++ src/stores/posts-store.ts
@@ -0,0 +1,44 @@
+import { defineStore } from "pinia";
+import { getPost, getPosts, getPostsTypes } from "src/api/posts";
+
+export const usePostsStore = defineStore("posts", {
+  state: () => ({
+    notLoaded: [] as PostMinimal[],
+    loaded: [] as Post[]
+  }),
+  getters: {},
+  actions: {
+    async getPosts(type: keyof typeof getPostsTypes) {
+      const r = await getPosts(type);
+      if (r.data.success) {
+        this.notLoaded = r.data.data.posts;
+      }
+      return r.data;
+    },
+    async loadPosts(count: number) {
+      const itemsToLoad = this.notLoaded.slice(0, count);
+
+      for (const item of itemsToLoad) {
+        const loaded = await getPost(item[0]);
+        if (loaded.data.success) this.loaded.push(loaded.data.data);
+      }
+
+      this.notLoaded = this.notLoaded.slice(count);
+    },
+    reset() {
+      this.notLoaded = [];
+      this.loaded = [];
+    },
+    viewPosts(count: number) {
+      if (count > this.loaded.length) {
+        const elements = [...this.loaded];
+        this.loaded = [];
+        return elements;
+      }
+
+      const elements = this.loaded.slice(-count);
+      this.loaded.splice(-count);
+      return elements;
+    }
+  }
+});
