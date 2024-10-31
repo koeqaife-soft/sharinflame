177e65330c4a0b58edde6f6dc01f00c6144b86fb
Danik <retikadni@gmail.com>
2024-11-01 00:06:13 +0100

    Import component

--- src/pages/AppPage.vue
+++ src/pages/AppPage.vue
@@ -1,7 +1,9 @@
 <template>
-  <q-page class="flex flex-center"></q-page>
+  <q-page class="flex flex-center">
+    <post-scroll></post-scroll>
+  </q-page>
 </template>
 
 <script setup lang="ts">
-//
+import PostScroll from "src/components/PostScroll.vue";
 </script>
