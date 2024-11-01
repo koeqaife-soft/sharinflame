adda55cf4f826f393f63ec46b3d8a2112863c404
Danik <retikadni@gmail.com>
2024-11-01 17:51:26 +0100

    PostScroll type

--- src/pages/AppPage.vue
+++ src/pages/AppPage.vue
@@ -1,9 +1,13 @@
 <template>
   <q-page class="flex flex-center">
-    <post-scroll></post-scroll>
+    <post-scroll :type="currentType" />
   </q-page>
 </template>
 
 <script setup lang="ts">
+import { KeyOfGetPostsTypes } from "src/api/posts";
 import PostScroll from "src/components/PostScroll.vue";
+import { ref } from "vue";
+
+const currentType = ref<KeyOfGetPostsTypes>("popular");
 </script>
