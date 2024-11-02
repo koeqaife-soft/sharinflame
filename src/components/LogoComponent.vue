53eb552c37e035623e6259f34fa7eacb55298d1a
Danik <retikadni@gmail.com>
2024-11-02 21:41:14 +0100

    Optimize logo

--- src/components/LogoComponent.vue
+++ src/components/LogoComponent.vue
@@ -1,22 +1,18 @@
 <template>
-  <svg
-    viewBox="-2.4 -2.4 28.8 28.8"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    :class="[iconClass, { animated: isAnimated }]"
-    @click="handleClick"
-  >
-    <defs>
-      <mask id="a">
-        <rect x="-2.4" y="-2.4" width="28.8" height="28.8" rx="14.4" fill="#fff" />
-        <path
-          d="M12 21c4.418 0 8-3.356 8-7.496 0-3.741-2.035-6.666-3.438-8.06-.26-.258-.694-.144-.84.189-.748 1.69-2.304 4.123-4.293 4.123-1.232.165-3.112-.888-1.594-6.107.137-.47-.365-.848-.749-.534C6.905 4.905 4 8.511 4 13.504 4 17.644 7.582 21 12 21"
-          fill="#000"
-        />
-      </mask>
-    </defs>
-    <rect x="-2.4" y="-2.4" width="28.8" height="28.8" rx="14.4" fill="currentColor" mask="url(#a)" />
-  </svg>
+  <div :class="[iconClass, { animated: isAnimated }]" @click="handleClick">
+    <svg viewBox="-2.4 -2.4 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <defs>
+        <mask id="a">
+          <rect x="-2.4" y="-2.4" width="28.8" height="28.8" rx="14.4" fill="#fff" />
+          <path
+            d="M12 21c4.418 0 8-3.356 8-7.496 0-3.741-2.035-6.666-3.438-8.06-.26-.258-.694-.144-.84.189-.748 1.69-2.304 4.123-4.293 4.123-1.232.165-3.112-.888-1.594-6.107.137-.47-.365-.848-.749-.534C6.905 4.905 4 8.511 4 13.504 4 17.644 7.582 21 12 21"
+            fill="#000"
+          />
+        </mask>
+      </defs>
+      <rect x="-2.4" y="-2.4" width="28.8" height="28.8" rx="14.4" fill="currentColor" mask="url(#a)" />
+    </svg>
+  </div>
 </template>
 <script setup lang="ts">
 interface Props {
