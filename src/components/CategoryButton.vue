08f0329bab7455c9e9a91cd767eac0fcf1dbeb28
Danik <retikadni@gmail.com>
2024-11-03 15:08:47 +0100

    Category button

---
+++ src/components/CategoryButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-btn
+    :icon="icon"
+    :label="label"
+    :class="['card-button', { selected: selected }, 'category-button']"
+    @click="handleClick"
+    unelevated
+    no-caps
+  />
+</template>
+<script setup lang="ts">
+export interface ButtonProps {
+  label: string;
+  icon: string;
+  selected?: boolean;
+  click: () => void;
+  type?: string;
+}
+
+const props = withDefaults(defineProps<ButtonProps>(), {
+  icon: "",
+  label: "",
+  selected: false
+});
+
+const handleClick = () => {
+  props.click();
+};
+</script>
