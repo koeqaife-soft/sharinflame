e7923061c2b5969e4702688bca57740660d95e85
Danik <retikadni@gmail.com>
2024-11-05 22:01:00 +0100

    Fix

--- src/components/UserAvatar.vue
+++ src/components/UserAvatar.vue
@@ -1,16 +1,16 @@
 <template>
-  <q-avatar class="avatar">
-    <template v-if="user.avatar_url">
-      <img :src="user.avatar_url" />
+  <q-avatar class="avatar" :key="user?.user_id || 0">
+    <template v-if="user?.avatar_url">
+      <img :src="user?.avatar_url" />
     </template>
     <template v-else>
-      {{ user.username.charAt(0).toUpperCase() }}
+      {{ user?.username.charAt(0).toUpperCase() }}
     </template>
   </q-avatar>
 </template>
 <script setup lang="ts">
 interface Props {
-  user: User;
+  user: User | undefined;
 }
 
 defineProps<Props>();
