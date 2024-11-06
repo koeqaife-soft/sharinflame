7d6a87376232c23ec5cd59eb16052a55f448ed6a
Danik <retikadni@gmail.com>
2024-11-06 11:51:50 +0100

    Open profile menu button

---
+++ src/components/OpenProfileMenu.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-btn class="profile webkit-no-drag" round flat>
+    <user-avatar :me="true" />
+    <q-menu class="menu-card">
+      <profile-menu buttons-class="card-button" />
+    </q-menu>
+  </q-btn>
+</template>
+<script setup lang="ts">
+import ProfileMenu from "./ProfileMenu.vue";
+import UserAvatar from "./UserAvatar.vue";
+</script>
