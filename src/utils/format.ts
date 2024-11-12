13e6ddcc0bab2f1d19ea49a9ba8caf591cbc5ef5
Danik <retikadni@gmail.com>
2024-11-12 21:31:22 +0100

    create formatStringForHtml function

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -14,4 +14,31 @@ function formatNumber(num: number): string {
   return num.toString();
 }
 
-export { formatNumber };
+function formatStringForHtml(str: string): string {
+  // NOTE: this code is for “just in case”, mostly the server will handle XSS attacks
+  const allowedTags = /^<\/?(i|b|strong|em|u|br)>$/;
+
+  str = str.replace(/<br>/g, "\n");
+
+  str = str.replace(/<\/?([a-zA-Z]+)[^>]*>|[^<]+/g, (match) => {
+    if (allowedTags.test(match)) {
+      return match;
+    }
+
+    const map: { [key: string]: string } = {
+      "&": "&amp;",
+      "<": "&lt;",
+      ">": "&gt;",
+      '"': "&quot;",
+      "'": "&#x27;"
+    };
+
+    return match.replace(/([&<>"'])/g, (m) => map[m] || m);
+  });
+
+  str = str.replace(/\n/g, "<br>");
+
+  return str;
+}
+
+export { formatNumber, formatStringForHtml };
