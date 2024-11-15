d84f232fae0e65f15606388dd850aece9a1c269b
Danik <retikadni@gmail.com>
2024-11-15 15:01:23 +0100

    Move interceptors to a separate file

---
+++ src/api/interceptors.ts
@@ -0,0 +1,141 @@
+import { AxiosError, AxiosResponse, isAxiosError, AxiosInstance } from "axios";
+import { useMainStore } from "src/stores/main-store";
+import { watch } from "vue";
+import router from "src/router";
+import { refresh } from "src/api/auth";
+
+let api: AxiosInstance;
+type mainStoreType = ReturnType<typeof useMainStore>;
+let mainStore: mainStoreType;
+
+const connectionInterceptor = () => {
+  const isConnectionError = (e: AxiosError) => {
+    return !e.response || e.code === "ECONNABORTED";
+  };
+
+  api.interceptors.response.use(
+    (response: AxiosResponse) => {
+      mainStore.setIsOffline(false);
+      return response;
+    },
+    async (error: AxiosError<unknown>) => {
+      const config = error.config as typeof error.config & { __retry: boolean };
+
+      if (config.__retry) return Promise.reject(error);
+      config.__retry = true;
+
+      if (isConnectionError(error)) {
+        mainStore.setIsOffline(true);
+        if (!config.url?.includes("/ping"))
+          return new Promise((resolve, reject) => {
+            const watching = watch(
+              () => mainStore.isOffline,
+              async (newValue) => {
+                if (!newValue) {
+                  let retryResponse: AxiosResponse<unknown, unknown> | undefined = undefined;
+                  try {
+                    retryResponse = await api(config);
+                    resolve(retryResponse);
+                    watching.stop();
+                  } catch (e) {
+                    if (isAxiosError(e) && isConnectionError(e)) {
+                      mainStore.setIsOffline(true);
+                    } else {
+                      watching.stop();
+                      reject(e);
+                    }
+                  }
+                }
+              },
+              { immediate: true }
+            );
+          });
+      } else {
+        mainStore.setIsOffline(false);
+      }
+
+      return Promise.reject(error);
+    }
+  );
+};
+
+const refreshInterceptor = () => {
+  let isRefreshing = false;
+  let refreshTimeout: NodeJS.Timeout | null = null;
+  let subscribers: Array<(success: boolean) => void> = [];
+
+  const onRefreshed = () => {
+    subscribers.forEach((callback) => callback(true));
+    subscribers = [];
+  };
+
+  const clearSubscribers = () => {
+    subscribers.forEach((callback) => callback(false));
+    subscribers = [];
+  };
+
+  const addSubscriber = (callback: (success: boolean) => void) => {
+    subscribers.push(callback);
+  };
+
+  api.interceptors.response.use(
+    (response: AxiosResponse) => response,
+    async (error: AxiosError<ApiResponse>) => {
+      const { response } = error;
+
+      if (response && response.status === 401 && response.data?.error === "EXPIRED_TOKEN") {
+        const originalRequest = error.config!;
+
+        if (!isRefreshing) {
+          isRefreshing = true;
+          let success = false;
+
+          try {
+            const refreshResponse = await refresh();
+            success = refreshResponse.data.success;
+
+            if (success) {
+              onRefreshed();
+              return api(originalRequest);
+            } else throw new Error("Unable to refresh token");
+          } catch (refreshError) {
+            clearSubscribers();
+            router.push({ path: "/login" });
+            return Promise.reject(refreshError);
+          } finally {
+            if (refreshTimeout) clearTimeout(refreshTimeout);
+            refreshTimeout = setTimeout(() => {
+              isRefreshing = false;
+              if (success) {
+                onRefreshed();
+              } else if (subscribers) {
+                clearSubscribers();
+              }
+            }, 10000);
+          }
+        }
+
+        return new Promise((resolve, reject) => {
+          addSubscriber((success: boolean) => {
+            if (success) resolve(api(originalRequest));
+            else reject("Unable to refresh token");
+          });
+        });
+      } else if (response && response.status === 401) {
+        clearSubscribers();
+        router.push({ path: "/login" });
+      }
+
+      return Promise.reject(error);
+    }
+  );
+};
+
+async function init(_api: AxiosInstance, _mainStore: mainStoreType) {
+  api = _api;
+  mainStore = _mainStore;
+  connectionInterceptor();
+  refreshInterceptor();
+}
+
+export { init, connectionInterceptor, refreshInterceptor };
--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -1,10 +1,9 @@
 import { boot } from "quasar/wrappers";
-import axios, { AxiosError, AxiosInstance, AxiosResponse, isAxiosError } from "axios";
-import { authEndpoints, refresh } from "src/api/auth";
+import axios, { AxiosInstance } from "axios";
+import { authEndpoints } from "src/api/auth";
 import { postsEndpoints } from "src/api/posts";
-import router from "src/router";
 import { useMainStore } from "src/stores/main-store";
-import { watch } from "vue";
+import * as interceptors from "src/api/interceptors";
 
 declare module "vue" {
   interface ComponentCustomProperties {
@@ -16,20 +15,23 @@ declare module "vue" {
 
 let mainStore: ReturnType<typeof useMainStore>;
 const url = "http://localhost:6169/v1";
-const api = axios.create({ baseURL: url, timeout: 5000, withCredentials: true });
+const api = axios.create({ baseURL: url, timeout: 15000, withCredentials: true });
 
-const initFunctions = [() => import("src/api/auth"), () => import("src/api/posts"), () => import("src/api/users")];
+const initFunctions = [
+  () => import("src/api/auth"),
+  () => import("src/api/posts"),
+  () => import("src/api/users"),
+  () => interceptors
+];
 
 async function initialize() {
   await Promise.all(
     initFunctions.map(async (initFunction) => {
       const module = await initFunction();
-      return module.init(api);
+      return module.init(api, mainStore);
     })
   );
 }
-
-initialize();
 
 const apiEndpoints = {
   auth: authEndpoints,
@@ -37,130 +39,12 @@ const apiEndpoints = {
   ping: "/ping"
 };
 
-let isRefreshing = false;
-let refreshTimeout: NodeJS.Timeout | null = null;
-let subscribers: Array<(success: boolean) => void> = [];
-
-const onRefreshed = () => {
-  subscribers.forEach((callback) => callback(true));
-  subscribers = [];
-};
-
-const clearSubscribers = () => {
-  subscribers.forEach((callback) => callback(false));
-  subscribers = [];
-};
-
-const addSubscriber = (callback: (success: boolean) => void) => {
-  subscribers.push(callback);
-};
-
-function isConnectionError(e: AxiosError) {
-  return !e.response || e.code === "ECONNABORTED";
-}
-
-api.interceptors.response.use(
-  (response: AxiosResponse) => {
-    mainStore.setIsOffline(false);
-    return response;
-  },
-  async (error: AxiosError<unknown>) => {
-    const config = error.config as typeof error.config & { __retry: boolean };
-
-    if (config.__retry) return Promise.reject(error);
-    config.__retry = true;
-
-    if (isConnectionError(error)) {
-      mainStore.setIsOffline(true);
-      if (!config.url?.includes("/ping"))
-        return new Promise((resolve, reject) => {
-          const watching = watch(
-            () => mainStore.isOffline,
-            async (newValue) => {
-              if (!newValue) {
-                let retryResponse: AxiosResponse<unknown, unknown> | undefined = undefined;
-                try {
-                  retryResponse = await api(config);
-                  resolve(retryResponse);
-                  watching.stop();
-                } catch (e) {
-                  if (isAxiosError(e) && isConnectionError(e)) {
-                    mainStore.setIsOffline(true);
-                  } else {
-                    watching.stop();
-                    reject(e);
-                  }
-                }
-              }
-            },
-            { immediate: true }
-          );
-        });
-    } else {
-      mainStore.setIsOffline(false);
-    }
-
-    return Promise.reject(error);
-  }
-);
-
-api.interceptors.response.use(
-  (response: AxiosResponse) => response,
-  async (error: AxiosError<ApiResponse>) => {
-    const { response } = error;
-
-    if (response && response.status === 401 && response.data?.error === "EXPIRED_TOKEN") {
-      const originalRequest = error.config!;
-
-      if (!isRefreshing) {
-        isRefreshing = true;
-        let success = false;
-
-        try {
-          const refreshResponse = await refresh();
-          success = refreshResponse.data.success;
-
-          if (success) {
-            onRefreshed();
-            return api(originalRequest);
-          } else throw new Error("Unable to refresh token");
-        } catch (refreshError) {
-          clearSubscribers();
-          router.push({ path: "/login" });
-          return Promise.reject(refreshError);
-        } finally {
-          if (refreshTimeout) clearTimeout(refreshTimeout);
-          refreshTimeout = setTimeout(() => {
-            isRefreshing = false;
-            if (success) {
-              onRefreshed();
-            } else if (subscribers) {
-              clearSubscribers();
-            }
-          }, 10000);
-        }
-      }
-
-      return new Promise((resolve, reject) => {
-        addSubscriber((success: boolean) => {
-          if (success) resolve(api(originalRequest));
-          else reject("Unable to refresh token");
-        });
-      });
-    } else if (response && response.status === 401) {
-      clearSubscribers();
-      router.push({ path: "/login" });
-    }
-
-    return Promise.reject(error);
-  }
-);
-
 export default boot(({ app }) => {
   app.config.globalProperties.$axios = axios;
   app.config.globalProperties.$api = api;
   app.config.globalProperties.$apiEndpoints = apiEndpoints;
   mainStore = useMainStore();
+  initialize();
 });
 
 export { api, apiEndpoints };
