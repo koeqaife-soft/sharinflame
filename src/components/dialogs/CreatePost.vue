fe63aef0b27c6e74d20a3c6197e01d5b1a5ca4b7
Danik <retikadni@gmail.com>
2024-12-14 20:33:03 +0100

    Add tags; Remove margins

--- src/components/dialogs/CreatePost.vue
+++ src/components/dialogs/CreatePost.vue
@@ -11,7 +11,7 @@
       <card-dialog-label class="q-mb-sm" icon="sym_r_article" :label="$t('create_post')">
         <q-btn flat round icon="sym_r_close" size="xs" @click="dialogRef!.hide()" />
       </card-dialog-label>
-      <q-separator class="separator" />
+      <q-separator class="separator q-mb-sm" />
       <q-scroll-area class="scroll-area fix-scroll-area" :visible="false">
         <q-input
           borderless
@@ -19,14 +19,56 @@
           autogrow
           maxlength="16384"
           :label="$t('add_your_text_here')"
-          class="full-width post-text card q-my-sm"
+          class="full-width post-text card"
           :disable="loading"
         />
-        <q-separator class="separator q-mb-sm" />
-        <toggle-value t_key="nsfw" icon="sym_r_explicit" v-model="is_nsfw" class="q-mb-sm" />
-        <toggle-value t_key="ai" icon="sym_r_robot_2" v-model="ai_generated" class="q-mb-sm" />
+        <div class="tags-container card">
+          <div class="horizontal-container">
+            <q-icon name="sym_r_tag" class="icon" />
+            <div class="label">{{ $t("tags") }}</div>
+          </div>
+          <div class="chips">
+            <q-chip
+              v-for="(tag, index) in tags"
+              :key="index"
+              removable
+              icon-remove="sym_r_close"
+              @remove="removeTag(index)"
+              class="tag"
+            >
+              {{ tag }}
+            </q-chip>
+            <q-chip clickable icon="sym_r_add" class="tag">
+              {{ $t("add_tag") }}
+              <q-menu class="menu-card enter-tag-menu">
+                <q-input
+                  v-model="addTagValue"
+                  :label="$t('enter_tag')"
+                  @keyup.enter="addTag"
+                  borderless
+                  class="tag-input full-width"
+                >
+                  <template v-slot:append>
+                    <q-btn
+                      round
+                      flat
+                      icon="sym_r_add"
+                      class="send-button"
+                      @click="addTag"
+                      :disable="addTagValue.length == 0"
+                    />
+                  </template>
+                </q-input>
+              </q-menu>
+            </q-chip>
+          </div>
+        </div>
+
+        <q-separator class="separator" />
+        <toggle-value t_key="nsfw" icon="sym_r_explicit" v-model="is_nsfw" />
+        <toggle-value t_key="ai" icon="sym_r_robot_2" v-model="ai_generated" />
       </q-scroll-area>
-      <q-separator class="separator q-mb-sm" />
+      <q-separator class="separator q-my-sm" />
       <div class="send-container">
         <q-btn
           class="default-button"
@@ -57,16 +99,20 @@ const quasar = useQuasar();
 const profileStore = useProfileStore();
 
 const text = ref<string>("");
+const addTagValue = ref<string>("");
+
 const is_nsfw = ref(false);
 const ai_generated = ref(false);
+
+const tags = ref<string[]>([]);
 
 const loading = ref(false);
 
 function generateTags() {
-  const tags = [];
-  if (is_nsfw.value) tags.push("is-nsfw");
-  if (ai_generated.value) tags.push("ai-generated");
-  return tags.length > 0 ? tags : undefined;
+  const generatedTags = tags.value;
+  if (is_nsfw.value) generatedTags.push("is-nsfw");
+  if (ai_generated.value) generatedTags.push("ai-generated");
+  return generatedTags.length > 0 ? generatedTags : undefined;
 }
 
 async function createPostButton() {
@@ -91,4 +137,15 @@ async function createPostButton() {
     dialogRef!.value?.hide();
   }
 }
+
+const addTag = () => {
+  if (addTagValue.value && !tags.value.includes(addTagValue.value)) {
+    tags.value.push(addTagValue.value);
+    addTagValue.value = "";
+  }
+};
+
+const removeTag = (index: number) => {
+  tags.value.splice(index, 1);
+};
 </script>
