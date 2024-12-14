1e3c293358fb166c25af2af868fb1c7be6accfbc
Danik <retikadni@gmail.com>
2024-12-14 13:54:24 +0100

    Create post; Button loading

--- src/components/dialogs/CreatePost.vue
+++ src/components/dialogs/CreatePost.vue
@@ -20,6 +20,7 @@
           maxlength="16384"
           :label="$t('add_your_text_here')"
           class="full-width post-text card q-my-sm"
+          :disable="loading"
         />
         <q-separator class="separator q-mb-sm" />
         <toggle-value t_key="nsfw" icon="sym_r_explicit" v-model="is_nsfw" class="q-mb-sm" />
@@ -27,24 +28,67 @@
       </q-scroll-area>
       <q-separator class="separator q-mb-sm" />
       <div class="send-container">
-        <q-btn class="default-button" icon-right="sym_r_add" :label="$t('create')" no-caps />
+        <q-btn
+          class="default-button"
+          icon-right="sym_r_add"
+          :label="$t('create')"
+          no-caps
+          @click="createPostButton"
+          :disable="text.length == 0"
+          :loading="loading"
+        />
       </div>
     </div>
   </q-dialog>
 </template>
 <script setup lang="ts">
-import { useDialogPluginComponent } from "quasar";
-import { ref, watch } from "vue";
+import { useDialogPluginComponent, useQuasar } from "quasar";
+import { defineAsyncComponent, ref } from "vue";
 import CardDialogLabel from "../misc/CardDialogLabel.vue";
 import ToggleValue from "./create-post/ToggleValue.vue";
+import { createPost } from "src/api/posts";
+import { useProfileStore } from "src/stores/profile-store";
 
 defineEmits([...useDialogPluginComponent.emits]);
 const { dialogRef, onDialogHide } = useDialogPluginComponent();
 
-const text = ref<string>();
+const PostDialog = defineAsyncComponent(() => import("./PostDialog.vue"));
+const quasar = useQuasar();
+const profileStore = useProfileStore();
+
+const text = ref<string>("");
 const is_nsfw = ref(false);
 const ai_generated = ref(false);
-watch(is_nsfw, () => {
-  console.log(is_nsfw.value);
-});
+
+const loading = ref(false);
+
+function generateTags() {
+  const tags = [];
+  if (is_nsfw.value) tags.push("is-nsfw");
+  if (ai_generated.value) tags.push("ai-generated");
+  return tags.length > 0 ? tags : undefined;
+}
+
+async function createPostButton() {
+  loading.value = true;
+  const tags = generateTags();
+  const r = await createPost({
+    content: text.value!,
+    tags: tags
+  });
+  loading.value = false;
+  if (r.data.success && r.data.data) {
+    const post = {
+      ...r.data.data,
+      user: await profileStore.getProfile()
+    };
+    quasar.dialog({
+      component: PostDialog,
+      componentProps: {
+        post: post
+      }
+    });
+    dialogRef!.value?.hide();
+  }
+}
 </script>
