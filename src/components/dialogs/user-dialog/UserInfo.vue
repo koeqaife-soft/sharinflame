44bef73add5fb0e941ab8445f1b3526119dce936
Danik <retikadni@gmail.com>
2024-12-25 01:12:01 +0100

    Languages

--- src/components/dialogs/user-dialog/UserInfo.vue
+++ src/components/dialogs/user-dialog/UserInfo.vue
@@ -1,20 +1,32 @@
 <template>
   <q-scroll-area class="scroll-area full-height fix-scroll-area" :visible="false">
-    <div class="card bio animation-fade-in-down q-mb-sm" v-if="user.bio">
-      <div class="header">
-        <q-icon name="sym_r_person" />
-        <div>{{ $t("about_me") }}</div>
+    <div class="container">
+      <div class="card bio animation-fade-in-down" v-if="user.bio">
+        <div class="header">
+          <q-icon name="sym_r_person" />
+          <div>{{ $t("about_me") }}</div>
+        </div>
+        <q-separator class="separator q-mb-xs q-mt-xs" />
+        <div class="text" v-html="formatStringForHtml(user.bio)" />
       </div>
-      <q-separator class="separator q-mb-xs q-mt-xs" />
-      <div class="text" v-html="formatStringForHtml(user.bio)" />
-    </div>
-    <div class="card created-at animation-fade-in-down" style="--anim-delay: 75ms">
-      <div class="header">
-        <q-icon name="sym_r_event" style="transform: translateY(-1.55px)" />
-        <div>{{ $t("created_at") }}</div>
+      <div class="card created-at animation-fade-in-down" style="--anim-delay: 75ms">
+        <div class="header">
+          <q-icon name="sym_r_event" style="transform: translateY(-1.55px)" />
+          <div>{{ $t("created_at") }}</div>
+        </div>
+        <q-separator class="separator q-mb-xs q-mt-xs" />
+        <div class="text">{{ formatUnixTime(user.created_at, $t("locale")) }}</div>
       </div>
-      <q-separator class="separator q-mb-xs q-mt-xs" />
-      <div class="text">{{ formatUnixTime(user.created_at, $t("locale")) }}</div>
+      <div class="card languages animation-fade-in-down" style="--anim-delay: 150ms" v-if="user.languages">
+        <div class="header">
+          <q-icon name="sym_r_language" />
+          <div>{{ $t("languages") }}</div>
+        </div>
+        <q-separator class="separator q-mb-xs q-mt-xs" />
+        <q-chip v-for="(item, index) in user.languages" :key="index">
+          {{ item }}
+        </q-chip>
+      </div>
     </div>
   </q-scroll-area>
 </template>
