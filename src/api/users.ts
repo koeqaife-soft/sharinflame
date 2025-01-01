6a527467769bcc9487676e15bf408c9722926243
Danik <retikadni@gmail.com>
2025-01-01 20:43:50 +0100

    Functions for favorites

--- src/api/users.ts
+++ src/api/users.ts
@@ -4,26 +4,33 @@ let api: AxiosInstance;
 
 export const usersEndpoints = {
   profile_me: "/users/me",
-  get_profile: (id: string) => `/users/${id}`
+  get_profile: (id: string) => `/users/${id}`,
+  favorite_posts: (id: string) => `/users/me/favorites/posts/${id}`,
+  favorite_comments: (id: string) => `/users/me/favorites/comments/${id}`
 };
 
 async function getProfile(user_id?: string) {
   if (!user_id) {
-    const r = await api.get<ResponseWithUser>(usersEndpoints.profile_me);
-    return r;
+    return await api.get<ResponseWithUser>(usersEndpoints.profile_me);
   } else {
-    const r = await api.get<ResponseWithUser>(usersEndpoints.get_profile(user_id));
-    return r;
+    return await api.get<ResponseWithUser>(usersEndpoints.get_profile(user_id));
   }
 }
 
 async function updateProfile(values: UpdateProfileValues) {
-  const r = await api.post(usersEndpoints.profile_me, values);
-  return r;
+  return await api.post(usersEndpoints.profile_me, values);
+}
+
+async function addPostToFavorites(id: string) {
+  return await api.post(usersEndpoints.favorite_posts(id));
+}
+
+async function remPostFromFavorites(id: string) {
+  return await api.delete(usersEndpoints.favorite_posts(id));
 }
 
 async function init(_api: AxiosInstance) {
   api = _api;
 }
 
-export { init, getProfile, updateProfile };
+export { init, getProfile, updateProfile, addPostToFavorites, remPostFromFavorites };
