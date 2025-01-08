2471dd1b0929958bd04150da3aaa208c7d7931db
Danik <retikadni@gmail.com>
2025-01-08 21:27:27 +0100

    Post skeleton

---
+++ src/components/skeletons/PostSkeleton.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-skeleton type="rect" :height="height" class="card post post-skeleton" />
+</template>
+<script setup lang="ts">
+import { randomSize } from "src/utils/random";
+
+const props = defineProps<{
+  height: string;
+}>();
+
+const height = props.height === "random" ? randomSize(125, 300) : props.height;
+</script>
