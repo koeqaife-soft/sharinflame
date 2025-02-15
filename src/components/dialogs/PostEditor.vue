28bf1d700c6fdc03e5695256ee344ce759788c43
Danik <retikadni@gmail.com>
2025-02-15 14:37:55 +0100

    Use header instead of label

--- src/components/dialogs/PostEditor.vue
+++ src/components/dialogs/PostEditor.vue
@@ -1,88 +1,90 @@
 <template>
   <q-dialog
-    transition-show="slide-up"
-    transition-hide="slide-down"
+    transition-show="scale"
+    transition-hide="scale"
     class="create-post-dialog card-dialog"
     ref="dialogRef"
     @hide="onDialogHide"
     maximized
   >
     <div class="dialog-content">
-      <card-dialog-label
-        class="q-mb-sm"
-        :icon="editMode ? 'sym_r_edit' : 'sym_r_article'"
-        :label="editMode ? $t('edit_post') : $t('create_post')"
-      >
-        <q-btn flat round icon="sym_r_close" size="xs" @click="dialogRef!.hide()" />
-      </card-dialog-label>
-      <q-separator class="separator q-mb-sm" />
-      <q-scroll-area class="scroll-area fix-scroll-area" :visible="false">
-        <q-input
-          borderless
-          v-model="text"
-          autogrow
-          maxlength="16384"
-          :label="$t('add_your_text_here')"
-          class="full-width post-text card"
-          :disable="loading"
-        />
-        <div class="tags-container card">
-          <div class="horizontal-container">
-            <q-icon name="sym_r_tag" class="icon" />
-            <div class="label">{{ $t("tags") }}</div>
-          </div>
-          <div class="chips">
-            <q-chip
-              v-for="(tag, index) in tags"
-              :key="index"
-              removable
-              icon-remove="sym_r_close"
-              @remove="removeTag(index)"
-              class="tag"
-            >
-              {{ tag }}
-            </q-chip>
-            <q-chip clickable icon="sym_r_add" class="tag" :disable="tags.length >= MAX_TAGS">
-              {{ $t("add_tag") }}
-              <q-menu class="menu-card enter-tag-menu" v-if="tags.length < MAX_TAGS">
-                <q-input
-                  v-model="addTagValue"
-                  :label="$t('enter_tag')"
-                  @keyup.enter="addTag"
-                  borderless
-                  class="tag-input full-width"
-                >
-                  <template v-slot:append>
-                    <q-btn
-                      round
-                      flat
-                      icon="sym_r_add"
-                      class="send-button"
-                      @click="addTag"
-                      :disable="addTagValue.length == 0"
-                    />
-                  </template>
-                </q-input>
-              </q-menu>
-            </q-chip>
-          </div>
+      <div class="card-dialog-header horizontal-container">
+        <div class="horizontal-container label-container">
+          <q-icon :name="editMode ? 'sym_r_edit' : 'sym_r_article'" class="header-icon" />
+          <div class="label">{{ editMode ? $t("edit_post") : $t("create_post") }}</div>
         </div>
+        <q-space />
+        <q-btn flat round icon="sym_r_close" @click="dialogRef?.hide()" />
+      </div>
+      <div class="dialog-content-inner">
+        <q-scroll-area class="scroll-area fix-scroll-area" :visible="false">
+          <q-input
+            borderless
+            v-model="text"
+            autogrow
+            maxlength="16384"
+            :label="$t('add_your_text_here')"
+            class="full-width post-text card"
+            :disable="loading"
+          />
+          <div class="tags-container card">
+            <div class="horizontal-container">
+              <q-icon name="sym_r_tag" class="icon" />
+              <div class="label">{{ $t("tags") }}</div>
+            </div>
+            <div class="chips">
+              <q-chip
+                v-for="(tag, index) in tags"
+                :key="index"
+                removable
+                icon-remove="sym_r_close"
+                @remove="removeTag(index)"
+                class="tag"
+              >
+                {{ tag }}
+              </q-chip>
+              <q-chip clickable icon="sym_r_add" class="tag" :disable="tags.length >= MAX_TAGS">
+                {{ $t("add_tag") }}
+                <q-menu class="menu-card enter-tag-menu" v-if="tags.length < MAX_TAGS">
+                  <q-input
+                    v-model="addTagValue"
+                    :label="$t('enter_tag')"
+                    @keyup.enter="addTag"
+                    borderless
+                    class="tag-input full-width"
+                  >
+                    <template v-slot:append>
+                      <q-btn
+                        round
+                        flat
+                        icon="sym_r_add"
+                        class="send-button"
+                        @click="addTag"
+                        :disable="addTagValue.length == 0"
+                      />
+                    </template>
+                  </q-input>
+                </q-menu>
+              </q-chip>
+            </div>
+          </div>
 
-        <q-separator class="separator" />
-        <toggle-value t_key="nsfw" icon="sym_r_explicit" v-model="is_nsfw" />
-        <toggle-value t_key="ai" icon="sym_r_robot_2" v-model="ai_generated" />
-      </q-scroll-area>
-      <q-separator class="separator q-my-sm" />
-      <div class="send-container">
-        <q-btn
-          class="default-button"
-          :icon-right="editMode ? 'sym_r_edit' : 'sym_r_add'"
-          :label="editMode ? $t('apply') : $t('create')"
-          no-caps
-          @click="buttonClick"
-          :disable="text.length == 0"
-          :loading="loading"
-        />
+          <q-separator class="separator" />
+          <toggle-value t_key="nsfw" icon="sym_r_explicit" v-model="is_nsfw" />
+          <toggle-value t_key="ai" icon="sym_r_robot_2" v-model="ai_generated" />
+        </q-scroll-area>
+        <q-separator class="separator q-my-sm" />
+        <div class="send-container">
+          <q-btn
+            class="default-button"
+            :icon-right="editMode ? 'sym_r_edit' : 'sym_r_add'"
+            :label="editMode ? $t('apply') : $t('create')"
+            no-caps
+            @click="buttonClick"
+            :disable="text.length == 0"
+            :loading="loading"
+          />
+        </div>
       </div>
     </div>
   </q-dialog>
@@ -90,7 +92,6 @@
 <script setup lang="ts">
 import { useDialogPluginComponent, useQuasar } from "quasar";
 import { defineAsyncComponent, onMounted, Ref, ref } from "vue";
-import CardDialogLabel from "../misc/CardDialogLabel.vue";
 import ToggleValue from "./create-post/ToggleValue.vue";
 import { createPost, editPost } from "src/api/posts";
 import { useProfileStore } from "src/stores/profile-store";
