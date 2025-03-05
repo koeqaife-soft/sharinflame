cbc4ee913d87b8370ed0ef8847b5ac15e45993ce
Danik <retikadni@gmail.com>
2025-03-05 15:10:48 +0100

    Move i18n inside of boot function

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -2,12 +2,12 @@ import { createI18n } from "vue-i18n";
 import messages from "src/i18n";
 import type { App } from "vue";
 
-export default ({ app }: { app: App }) => {
-  const i18n = createI18n({
-    locale: "en-US",
-    legacy: false,
-    messages
-  });
+export const i18n = createI18n({
+  locale: "en-US",
+  legacy: false,
+  messages
+});
 
+export default ({ app }: { app: App }) => {
   app.use(i18n);
 };
