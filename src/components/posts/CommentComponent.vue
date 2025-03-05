1a9c2397fa9499b9ad0de2722cea714d0aeb6c4f
Danik <retikadni@gmail.com>
2025-03-05 20:43:53 +0100

    Fix; Use async components

--- src/components/posts/CommentComponent.vue
+++ src/components/posts/CommentComponent.vue
@@ -1,17 +1,19 @@
 <template>
   <q-card class="comment card" unelevated :key="commentRef.comment_id">
     <q-card-section class="q-pb-none">
-      <open-user-dialog :user="commentRef.user" />
+      <div class="avatar-container">
+        <open-user-dialog :user="commentRef.user" />
+      </div>
       <div class="text-container">
         <div class="username">
           {{ commentRef.user.display_name || commentRef.user.username }}
         </div>
-        <div class="content wrap-text" v-html="formatStringForHtml(commentRef.content)" />
+        <text-parts :text="formatStringForHtml(commentRef.content)" :html="true" />
       </div>
     </q-card-section>
     <q-card-actions class="actions" :class="{ 'can-animate': canAnimate }">
       <div class="reaction-buttons">
-        <reaction-buttons :object="commentRef" :before-action="allowAnimate" />
+        <reaction-buttons :object="commentRef" :before-action="allowAnimate" :is-comment="true" />
       </div>
     </q-card-actions>
   </q-card>
@@ -19,9 +21,10 @@
 
 <script setup lang="ts">
 import { defineAsyncComponent, ref, toRef } from "vue";
-import OpenUserDialog from "../dialogs/OpenUserDialog.vue";
 import { formatStringForHtml } from "src/utils/format";
+import TextParts from "../misc/TextParts.vue";
 
+const OpenUserDialog = defineAsyncComponent(() => import("../dialogs/OpenUserDialog.vue"));
 const ReactionButtons = defineAsyncComponent(() => import("./ReactionButtons.vue"));
 
 const props = defineProps<{
