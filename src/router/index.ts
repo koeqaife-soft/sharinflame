2fc7c997ea6445f28f4526027570554fcf81f294
Danik <retikadni@gmail.com>
2025-03-14 21:53:34 +0100

    Format with prettier

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,8 +6,8 @@ import routes from "./routes";
 const createHistory = process.env.SERVER
   ? createMemoryHistory
   : process.env.VUE_ROUTER_MODE === "history"
-  ? createWebHistory
-  : createWebHashHistory;
+    ? createWebHistory
+    : createWebHashHistory;
 
 const router = createRouter({
   scrollBehavior: () => ({ left: 0, top: 0 }),
