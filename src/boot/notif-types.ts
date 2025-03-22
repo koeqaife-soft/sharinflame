8e6910fb848c55a67fb78385111181a3340939cd
Danik <retikadni@gmail.com>
2025-03-22 17:31:07 +0100

    Close button

--- src/boot/notif-types.ts
+++ src/boot/notif-types.ts
@@ -5,7 +5,8 @@ export default defineBoot(() => {
   Notify.registerType("default-notification", {
     icon: "sym_r_notifications",
     classes: "default-notification",
-    position: "top-right"
+    position: "top-right",
+    actions: [{ icon: "sym_r_close", round: true, handler: () => {} }]
   });
   Notify.registerType("error-notification", {
     icon: "sym_r_error",
