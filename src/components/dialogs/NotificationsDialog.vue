c08deef23e071bd45f3432462f1a25b06e52391d
Danik <retikadni@gmail.com>
2025-04-15 20:30:56 +0200

    New dialog: NotificationsDialog

---
+++ src/components/dialogs/NotificationsDialog.vue
@@ -0,0 +1,109 @@
+<template>
+  <q-dialog
+    transition-show="scale"
+    transition-hide="scale"
+    class="notifications-dialog card-dialog"
+    ref="dialogRef"
+    @hide="onDialogHide"
+    maximized
+  >
+    <div class="dialog-content">
+      <div class="card-dialog-header horizontal-container">
+        <div class="horizontal-container label-container">
+          <q-icon name="sym_r_notifications" class="header-icon" />
+          <div class="label">{{ $t("notifications.label") }}</div>
+        </div>
+        <q-space />
+        <q-btn flat round icon="sym_r_close" @click="dialogRef?.hide()" />
+      </div>
+      <div class="dialog-content-inner">
+        <q-scroll-area class="scroll-area fix-scroll-area full-height" :visible="false">
+          <div class="no-notifications" v-if="showNoNotifications">{{ $t("no_notifications") }}</div>
+
+          <my-virtual-scroll
+            :items="items"
+            :margins="8"
+            item-key="id"
+            @load-more="loadMore"
+            infinite-load-type="bottom"
+            :key="scrollKey"
+            class="posts-infinite-scroll"
+            ref="virtualScroll"
+            :min-item-height="60"
+          >
+            <template v-slot:default="{ item }">
+              <notification-card :notif="item" :cache="notifCache" class="q-mb-sm" />
+            </template>
+            <template v-slot:loading>
+              <div class="row justify-center q-my-md">
+                <q-spinner class="loading full-height q-my-md" size="40px" />
+              </div>
+            </template>
+          </my-virtual-scroll>
+        </q-scroll-area>
+      </div>
+    </div>
+  </q-dialog>
+</template>
+<script setup lang="ts">
+import { useDialogPluginComponent } from "quasar";
+import { defineAsyncComponent, type DefineComponent, onMounted, ref, watch } from "vue";
+import type { CacheType } from "src/components/notifications/NotificationCard.vue";
+import MyVirtualScroll from "../misc/MyVirtualScroll.vue";
+import { getNotifications } from "src/api/users";
+import { truncate } from "src/utils/format";
+import { useMainStore } from "src/stores/main-store";
+
+const NotificationCard = defineAsyncComponent(() => import("src/components/notifications/NotificationCard.vue"));
+
+defineEmits([...useDialogPluginComponent.emits]);
+const { dialogRef, onDialogHide } = useDialogPluginComponent();
+
+const mainStore = useMainStore();
+
+const showNoNotifications = ref(false);
+
+const virtualScroll = ref<DefineComponent | null>(null);
+const scrollKey = ref(Date.now());
+const items = ref<ApiNotification[]>([]);
+
+const notifCache = ref<Record<string, CacheType>>({});
+let cursor: string | undefined = undefined;
+
+watch(
+  mainStore.lastNotifications,
+  (newVal) => {
+    newVal.forEach((value) => {
+      if (items.value.some((v) => value.id === v.id)) return;
+      items.value.unshift(value);
+      virtualScroll.value?.updateShowedItems();
+    });
+  },
+  { deep: false, immediate: false }
+);
+
+async function loadMore(index: number, done: (stop?: boolean) => void) {
+  try {
+    const r = await getNotifications(cursor, 25);
+    if (r.data.success) {
+      cursor = r.data.data.next_cursor;
+      r.data.data.notifications.forEach((value) => {
+        if (value.message) value.message = truncate(value.message, 256);
+        if (value.loaded?.content) value.loaded.content = truncate(value.loaded.content, 256);
+      });
+      items.value.push(...r.data.data.notifications);
+      done(!r.data.data.has_more);
+    } else {
+      done(true);
+    }
+  } catch (e) {
+    done(true);
+    throw e;
+  }
+}
+
+onMounted(() => {
+  mainStore.openedDialogs.notifications?.();
+  mainStore.openedDialogs.notifications = dialogRef.value!.hide;
+});
+</script>
