2c4864a126049981b6eecb5285c384992eff5021
Danik <retikadni@gmail.com>
2025-04-15 20:31:24 +0200

    Some optimizations

--- src/components/notifications/NotificationsList.vue
+++ src/components/notifications/NotificationsList.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="notifications">
     <div class="notifications-container">
-      <div v-if="loading" class="loading container">
+      <div v-if="loading && showLoading" class="loading container">
         <rect-skeleton v-for="i in 5" :key="i" class="notification-skeleton" height="60px" />
       </div>
-      <template v-else>
-        <div v-if="!loading && notifications.length === 0" class="no-notifications">
+      <template v-else-if="!loading">
+        <div v-if="notifications.length === 0" class="no-notifications">
           {{ $t("notifications.no_notifications") }}
         </div>
         <transition-group name="notification" tag="div" class="container">
@@ -19,6 +19,7 @@
 import { defineAsyncComponent, onMounted, ref, watch } from "vue";
 import { useMainStore } from "src/stores/main-store";
 import { getNotifications } from "src/api/users";
+import { truncate } from "src/utils/format";
 
 const mainStore = useMainStore();
 
@@ -27,33 +28,40 @@ const RectSkeleton = defineAsyncComponent(() => import("src/components/skeletons
 
 const notifications = ref<ApiNotification[]>([]);
 const loading = ref(true);
-
-const truncate = (text: string, length: number) => (text?.length > length ? `${text.slice(0, length - 3)}...` : text);
+const showLoading = ref(false);
+let showLoadingTimeout: NodeJS.Timeout | null = null;
 
 watch(
   mainStore.lastNotifications,
   (newVal) => {
     notifications.value = newVal.map((value) => {
+      const old = notifications.value.find((v) => value.id === v.id);
+      if (old) return old;
+
       if (value.message) value.message = truncate(value.message, 32);
       if (value.loaded?.content) value.loaded.content = truncate(value.loaded.content, 32);
       return value;
     });
     if (newVal.length > 0) {
       loading.value = false;
+      showLoading.value = false;
     }
   },
   { immediate: true }
 );
 
 onMounted(() => {
+  showLoadingTimeout = setTimeout(() => {
+    showLoading.value = true;
+  }, 150);
   if (mainStore.lastNotifications.length === 0) {
     void getNotifications(undefined, 5).then((response) => {
+      if (showLoadingTimeout) clearTimeout(showLoadingTimeout);
       if (response.data.success) {
         response.data.data.notifications.reverse().forEach((notif) => {
           mainStore.addNotification(notif);
         });
       }
-      loading.value = false;
     });
   }
 });
