2e32c2529df8a677b524b03c0e4261253c303b5a
Danik <retikadni@gmail.com>
2025-09-13 13:19:26 +0200

    Cancel loading on unmount

--- src/components/my/MyImage.vue
+++ src/components/my/MyImage.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="my-image">
-    <img :src="src" loading="lazy" />
+    <img :src="src" loading="lazy" ref="imgRef" />
   </div>
 </template>
 <script setup lang="ts">
+import { onBeforeUnmount, ref } from "vue";
+
 defineProps<{
   src: string;
 }>();
+
+const imgRef = ref<HTMLImageElement | null>(null);
+
+onBeforeUnmount(() => {
+  if (imgRef.value) {
+    imgRef.value.src = "";
+  }
+});
 </script>
