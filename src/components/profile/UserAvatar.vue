85c9d270a0151ecd13d4691d0630478bb67d755a
Danik <retikadni@gmail.com>
2025-10-31 14:20:36 +0100

    Better avatar

--- src/components/profile/UserAvatar.vue
+++ src/components/profile/UserAvatar.vue
@@ -1,16 +1,19 @@
 <template>
-  <q-avatar class="avatar" :key="userRef?.user_id || 0">
-    <template v-if="userRef?.avatar_url">
-      <img :src="userRef?.avatar_url" />
-    </template>
-    <template v-else>
-      {{ userRef?.username.charAt(0).toUpperCase() }}
-    </template>
-  </q-avatar>
+  <div ref="avatarRef" class="avatar" :key="userRef?.user_id || 0">
+    <div class="avatar-content">
+      <template v-if="userRef?.avatar_url">
+        <my-image :src="userRef?.avatar_url" />
+      </template>
+      <template v-else>
+        {{ userRef?.username.charAt(0).toUpperCase() }}
+      </template>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
+import MyImage from "../my/MyImage.vue";
 import { useProfileStore } from "src/stores/profile-store";
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 
 interface Props {
   user?: User | undefined;
@@ -20,13 +23,18 @@ const props = defineProps<Props>();
 
 const profileStore = useProfileStore();
 const userRef = ref<User>();
+const avatarRef = ref<HTMLElement | null>(null);
 
 async function loadUser() {
   userRef.value = await profileStore.getProfile();
 }
 
-onMounted(() => {
+function onLoad() {
   if (props.user) userRef.value = props.user;
   else if (props.me) void loadUser();
-});
+}
+
+watch(props, onLoad);
+
+onMounted(onLoad);
 </script>
